44f9c67a1af9287c4eaac29055dd2cc2fa9ae59f
Oskar Lindgren <2677573+MrOggy85@users.noreply.github.com>
2022-11-15 07:56:04 +0900

    chore: update deps

--- src/deps.ts
+++ src/deps.ts
@@ -11,7 +11,7 @@ export {
   PostgresError,
 } from "https://deno.land/x/postgres@v0.14.2/mod.ts";
 
-export { v4 as uuid } from "https://deno.land/std@0.161.0/uuid/mod.ts";
+export { v4 as uuid } from "https://deno.land/std@0.164.0/uuid/mod.ts";
 export {
   Database,
   DataTypes,
@@ -19,11 +19,9 @@ export {
   PostgresConnector,
   Relationships,
   SQLite3Connector,
-  // TODO: Switch to deno.land when new version is released
-  // } from "https://deno.land/x/denodb@v1.0.40/mod.ts";
-} from "https://raw.githubusercontent.com/eveningkid/denodb/3a384bb1e5e77367ee036cf925d9b12de0277b0a/mod.ts";
+} from "https://deno.land/x/denodb@v1.1.0/mod.ts";
 
-export { createHash } from "https://deno.land/std@0.161.0/node/crypto.ts";
+export { createHash } from "https://deno.land/std@0.164.0/node/crypto.ts";
 
 export { parseCronExpression } from "https://cdn.skypack.dev/cron-schedule@3.0.6?dts";
 
